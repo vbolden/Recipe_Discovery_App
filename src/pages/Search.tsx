99cac432f15c8cb228bf3bce20ccb235f2e9cf66
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:19:09 -0400

    added imports and created SearchPage function

---
+++ src/pages/Search.tsx
@@ -0,0 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import type { MealsResponse } from "../types/recipe";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/Errors";
+
+function SearchPage() {
+
+}
+
+export default SearchPage;
