cc556ab200f08f5b142bf44810083a43b6b9165f
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 14:44:23 -0400

    added API fetch with useFetch; commented out conditional including components that aren't finished and added UI rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
-import { CategoriesResponse } from "../types/recipe";
+import type { CategoriesResponse } from "../types/recipe";
+import { endpoints } from "../api/mealdb";
+import Spinner from "../components/Spinner";
+// import Errors from "../components/Errors";
 
 function HomePage() {
+    const {data, loading, error} = useFetch<CategoriesResponse>(endpoints.categories);
 
+    // if(loading) return <Spinner />
+    // if(error) return <ErrorMessage />
+
+    return (
+        <div>
+            {data?.categories.map(category => (
+                <Link key={category.id}
+                to={`/category/${category.strCategory}`} >
+                    <div>
+                        <img src={category.strCategoryThumb} alt={category.strCategory} />
+                        <h3>{category.strCategory}</h3>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
 }
 
 export default HomePage;
