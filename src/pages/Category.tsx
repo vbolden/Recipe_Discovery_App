b4abf963d1c0ead636c9b68ba0968d07631de86a
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 15:02:16 -0400

    added imports and function CategoryPage

---
+++ src/pages/Category.tsx
@@ -0,0 +1,11 @@
+import { useParams, Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import type { MealsResponse } from "../types/recipe";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/Errors";
+
+function CategoryPage() {
+
+}
+
+export default CategoryPage;
