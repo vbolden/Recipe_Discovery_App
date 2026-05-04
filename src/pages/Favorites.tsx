061315dbce699d24632934c3327c83f284cc50e5
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:49:29 -0400

    added async function to fetch recipe details of favorited meals

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useFavorites } from "../context/FavoritesContext";
 import type { MealDetail } from "../types/recipe";
+import { endpoints } from "../api/mealdb";
 
 function FavoritesPage() {
     const { favorites, removeFavorite } = useFavorites();
@@ -14,8 +15,25 @@ function FavoritesPage() {
             return;
         }
 
-        
-    })
+        async function fetchFavorites() {
+            const fetchedMeals: MealDetail[] = [];
+
+            for (const id of favorites) {
+                const response = await fetch(
+                    endpoints.recipeDetails(id)
+                );
+                const data = await response.json();
+
+                const meal = data.meals?.[0];
+
+                if(meal) {
+                    fetchedMeals.push(meal);
+                }
+            }
+            setMeals(fetchedMeals)
+        }
+        fetchFavorites();
+    }, [favorites])
 }
 
 export default FavoritesPage;
