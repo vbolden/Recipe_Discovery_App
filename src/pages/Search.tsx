1a98c4686431253be5b274b27f2759663b8ce617
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:23:20 -0400

    added query variable and searchParams; added conditionals for useFetch, data, and error

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,9 +3,20 @@ import useFetch from "../hooks/useFetch";
 import type { MealsResponse } from "../types/recipe";
 import Spinner from "../components/Spinner";
 import ErrorMessage from "../components/Errors";
+import { endpoints } from "../api/mealdb";
 
 function SearchPage() {
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get('query');
 
+    const {data, loading, error} = useFetch<MealsResponse>(
+        query
+        ? endpoints.searchMeals(query)
+        : null
+    );
+
+    if (loading) return <Spinner />
+    if (error) return <ErrorMessage message={error} />
 }
 
 export default SearchPage;
