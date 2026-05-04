a6424d3c236dfc284897033333ff2cad9964e085
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 10:11:52 -0400

    created useStates for data, loading, and errors

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 
 function useFetch<T>(url: string) {
-
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 }
 
 export default useFetch;
