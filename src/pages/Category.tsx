4ea2d916c3f712be08200aa5d93b20b8585937a8
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 15:26:43 -0400

    added UI render for recipe cards

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -10,12 +10,28 @@ function CategoryPage() {
 
     const { data, loading, error } = useFetch<MealsResponse>(
         categoryName
-        ? endpoints.categoryMeals(categoryName)
-        : null
+            ? endpoints.categoryMeals(categoryName)
+            : null
     );
 
-    if(loading) return <Spinner />;
-    if(error) return <ErrorMessage message={error} />
+    if (loading) return <Spinner />;
+    if (error) return <ErrorMessage message={error} />
+
+    return (
+        <div>
+            <h2>{categoryName}</h2>
+            <div className="grid">
+                {data?.meals?.map(meal => (
+                    <Link key={meal.idMeal} to={`/recipe/${meal.idMeal}`} >
+                        <div className="recipe-card">
+                            <img src={meal.strMealThumb} alt={meal.strMeal} />
+                            <h4>{meal.idMeal}</h4>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default CategoryPage;
