bc2fe66185d43d45a44a4a744554f7135ca6c5c2
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 15:22:25 -0400

    added conditionals for loading and error

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,11 +1,21 @@
 import { useParams, Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import type { MealsResponse } from "../types/recipe";
+import { endpoints } from "../api/mealdb";
 import Spinner from "../components/Spinner";
 import ErrorMessage from "../components/Errors";
 
 function CategoryPage() {
+    const { categoryName } = useParams<{ categoryName: string }>();
 
+    const { data, loading, error } = useFetch<MealsResponse>(
+        categoryName
+        ? endpoints.categoryMeals(categoryName)
+        : null
+    );
+
+    if(loading) return <Spinner />;
+    if(error) return <ErrorMessage message={error} />
 }
 
 export default CategoryPage;
