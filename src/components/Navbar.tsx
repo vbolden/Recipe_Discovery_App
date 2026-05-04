e2b02ddbfcc4ec2edfcad892224803b4c38b4246
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 17:16:48 -0400

    added UI render for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,30 @@ function Navbar() {
 
     const handleSearch = (e: React.SubmitEvent) => {
         e.preventDefault();
-        
-        if(!query.trim()) return;
+
+        if (!query.trim()) return;
 
         navigate(`/search?query=${encodeURIComponent(query.trim())}`);
         setQuery('')
     };
+
+    return (
+        <nav>
+            <Link to="/">Recipe Discovery</Link>
+            <form onSubmit={handleSearch}>
+                <input
+                    value={query}
+                    type="text"
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Search recipes..."
+                />
+                <button type="submit">
+                    Search
+                </button>
+            </form>
+            <Link to="/favorites" >Favorites</Link>
+        </nav>
+    )
 }
 
 export default Navbar;
