36b591d72299affe9f651b98650b66306e4c6176
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 14:28:16 -0400

    created basic routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FavoritesProvider } from './context/FavoritesContext'
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <FavoritesProvider>
+      {/* <Navbar /> */}
+      <div className="container">
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </div>
+    </FavoritesProvider>
+    </BrowserRouter>
   )
 }
 
