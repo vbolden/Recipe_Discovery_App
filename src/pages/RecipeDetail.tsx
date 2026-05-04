fae2f96c221a1e0f04bda58e2c8bc2c7ac177425
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 15:29:34 -0400

    add imports and RecipeDetailPage function

---
+++ src/pages/RecipeDetail.tsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { useFavorites } from "../context/FavoritesContext";
+import type { MealDetailResponse } from "../types/recipe";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/Errors";
+
+function RecipeDetailPage() {
+
+}
+
+export default RecipeDetailPage;
