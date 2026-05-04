78c47f582c254a4d3d8f8e661cf5d0d26c969edb
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:42:01 -0400

    added variables for useFavorites and useState

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -4,7 +4,18 @@ import { useFavorites } from "../context/FavoritesContext";
 import type { MealDetail } from "../types/recipe";
 
 function FavoritesPage() {
+    const { favorites, removeFavorite } = useFavorites();
+    const [meals, setMeals] = useState<MealDetail[]>([]);
 
+    // FETCH DETAILS FOR EACH MEAL ID
+    useEffect(() => {
+        if (favorites.length === 0) {
+            setMeals([]);
+            return;
+        }
+
+        
+    })
 }
 
 export default FavoritesPage;
