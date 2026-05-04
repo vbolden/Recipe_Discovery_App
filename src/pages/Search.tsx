524b128339e958cf6a013acf92b708c2dba84178
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:27:54 -0400

    added UI render for search results

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import type { MealsResponse } from "../types/recipe";
 import Spinner from "../components/Spinner";
@@ -17,6 +17,28 @@ function SearchPage() {
 
     if (loading) return <Spinner />
     if (error) return <ErrorMessage message={error} />
+
+    const meals = data?.meals;
+
+    return(
+        <div>
+            <h2>Results for "{query}"</h2>
+            {!meals ? (
+                <p>No recipes found. Try a different search</p>
+            ) : (
+                <div>
+                    {meals.map(meal => (
+                        <Link key={meal.idMeal} to={`/recipe/${meal.idMeal}`} >
+                            <div className="recipe-card">
+                                <img src={meal.strMealThumb} alt={meal.idMeal} />
+                                <h4>{meal.strMeal}</h4>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default SearchPage;
