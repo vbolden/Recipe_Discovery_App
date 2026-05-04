8c0c86081925da9731bcf306bd803346c6c59c61
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 15:20:46 -0400

    fixed typing error for url by checking if url is null and storing the value if url is not null

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 
-function useFetch<T>(url: string) {
+function useFetch<T>(url: string | null) {
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
     // USEEFFECT STATEMENT
     useEffect(() => {
+        if(!url) return;
+        // STORED URL IN VARIABLE AFTER NULL CHECK
+        const fetchUrl = url;
+
         // API FETCH, TRY/CATCH BLOCK
         async function fetchData() {
             try {
                 setLoading(true);
 
-                const response = await fetch(url);
+                const response = await fetch(fetchUrl);
 
                 if (!response.ok) {
                     throw new Error("Something went wrong...");
