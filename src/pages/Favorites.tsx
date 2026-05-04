807fc7e141922c20e34eb36cf755763000e53125
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 16:38:05 -0400

    added imports and created FavoritesPage function

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useFavorites } from "../context/FavoritesContext";
+import type { MealDetail } from "../types/recipe";
+
+function FavoritesPage() {
+
+}
+
+export default FavoritesPage;
