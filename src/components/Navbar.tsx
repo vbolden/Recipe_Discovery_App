9813ceb80d730db1b1791c654640613ccfe0f925
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 17:13:12 -0400

    created handleSearch function

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { endpoints } from "../api/mealdb";
 
 function Navbar() {
-    
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        
+        if(!query.trim()) return;
+
+        navigate(`/search?query=${encodeURIComponent(query.trim())}`);
+        setQuery('')
+    };
 }
 
 export default Navbar;
