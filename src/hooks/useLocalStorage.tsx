7aee4a2a3a54021d2cc88f053e86803eda4b920e
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-04 10:41:09 -0400

    added useLocalStorage function with conditional to get stored data or use initalValue

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -2,5 +2,9 @@
 import { useEffect, useState } from "react";
 
 function useLocalStorage<T>( key: string, initialValue: T) {
-    
+    const [value, setValue] = useState<T>(() => {
+        const stored = localStorage.getItem(key);
+
+        return stored ? JSON.parse(stored) : initialValue
+    })
 }
