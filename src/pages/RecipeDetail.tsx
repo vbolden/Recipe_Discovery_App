e764931052e18dba980d979e8390140b506088e0
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-05 09:13:13 -0400

    added class names for styling and fixed measure variable to show ingredient measurement

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -27,7 +27,7 @@ function RecipeDetailPage() {
 
     for (let i = 1; i <= 20; i++) {
         const ingredient = meal[`strIngredient${i}`];
-        const measure = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
 
         if (ingredient && ingredient.trim() !== "") {
             ingredients.push({
