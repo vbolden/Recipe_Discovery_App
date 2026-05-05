6b7d159e88cff69ca9fae62ceb28d04377ffba56
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-05 10:48:08 -0400

    replaced link with RecipeCard component

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,9 +1,10 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import type { MealsResponse } from "../types/recipe";
 import { endpoints } from "../api/mealdb";
 import Spinner from "../components/Spinner";
 import ErrorMessage from "../components/Errors";
+import RecipeCard from "../components/RecipeCard";
 
 function CategoryPage() {
     const { categoryName } = useParams<{ categoryName: string }>();
@@ -22,12 +23,7 @@ function CategoryPage() {
             <h2>{categoryName}</h2>
             <div className="grid">
                 {data?.meals?.map(meal => (
-                    <Link key={meal.idMeal} to={`/recipe/${meal.idMeal}`} >
-                        <div className="recipe-card">
-                            <img src={meal.strMealThumb} alt={meal.strMeal} />
-                            <h4>{meal.idMeal}</h4>
-                        </div>
-                    </Link>
+                    <RecipeCard key={meal.idMeal} meal={meal} />
                 ))}
             </div>
         </div>
