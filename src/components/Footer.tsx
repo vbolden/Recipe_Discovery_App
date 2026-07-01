e0eabba8a36015059d5e366a0639e1fc19b167e9
Valerie Bolden <valerie.bolden@gmail.com>
2026-07-01 10:57:11 -0400

    created a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <h3>
+                fork<span>&</span>find
+            </h3>
+            <p>
+                Discover recipes • Save favorites • Cook something new 
+            </p>
+            <small>
+                Built with React + TheMealDB API
+            </small>
+            <small>
+                &copy;2026 Valerie Bolden
+            </small>
+        </footer>
+    )
+}
+
+export default Footer;
